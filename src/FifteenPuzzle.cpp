ea8ed5971de15630aebbbbddd97fd261e7f26c3b
Chakrone Anas <chakrone.anas@gmail.com>
2025-04-19 23:03:43 +0100

    added moveTile function, the core game mechanic

--- src/FifteenPuzzle.cpp
+++ src/FifteenPuzzle.cpp
@@ -5,15 +5,44 @@ using namespace std;
 #include <vector>
 
 
-FifteenPuzzle::FifteenPuzzle() {
+FifteenPuzzle::FifteenPuzzle() { // @suppress("Class members should be properly initialized")
 	this->board = vector<vector<int>>(this->size, vector<int>(this->size));
 
 	int value = 1;
 	for (int i=0; i<size; i++){
 		for (int j=0; j<size; j++){
-			board[i][j]==value++
+			board[i][j]==value++;
 		}
 	}
 
+	board[this->size-1][this->size] == 0;
+	this->EmptyCol = size-1;
+	this->EmptyRow = size-1;
+
+}
+
+bool FifteenPuzzle::moveTile(char key){
+	int newRow = this->EmptyRow;
+	int newCol = this->EmptyCol;
+
+	switch(char direction) {
+	case 'W': case 'w' : newRow--; break;
+	case 'S': case 's' : newRow++; break;
+	case 'A': case 'a' : newCol--; break;
+	case 'D': case 'd' : newCol++; break;
+	default : return false;
+	}
+
+	// check if move is valid
+	if (newRow<0 || newRow>this->size-1 || newCol<0 || newCol>this->size) {
+		return false;
+	}
+
+	this->board[this->EmptyRow][this->EmptyCol] = this->board[newRow][newCol];
+	this->board[newRow][newCol] = 0;
+	this->EmptyRow = newRow;
+	this->EmptyCol = newCol;
+
+	return true;
 }
 
